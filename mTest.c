1335840c9c0952c633803517d694b9c24848732a
Lelouch <LelouchHe@gmail.com>
2011-01-09 01:00:22 +0800

    0.01

--- mTest.c
+++ mTest.c
@@ -1,31 +1,6 @@
 #include "mTest.h"
 #include <stdio.h>
 
-#define TRUE 1
-#define FALSE 0
+#define TESTMAX 10
 
-#define EXPECT_EQ(EXPECTED, ACTUAL)
-struct Expect_##EXPECTED
-{
-    int expected;
-    int actual;
-    char* name;
-};
-
-struct Expect_##EXPECTED_##ACTUAL* e = malloc(sizeof (e));
-e->name = #EXPECTED;
-e->expected = EXPECTED;
-e->actual = ACTUAL;
-
-int run(struct Expect_##EXPECTED* e)
-{
-    if (e->expected == e->actual)
-    {
-        printf("Expectation %s : PASS", e->name);
-        return TRUE;
-    else
-    {
-        printf("Expectation %s : FAIL\nExpected = %d\nActual = %d\b", e->name, e->expected, e->actual);
-        return FALSE;
-    }
-}
+struct TestTable table[TESTMAX];
--- mTest.h
+++ mTest.h
@@ -1,11 +1,78 @@
 #ifndef MTEST_H_INCLUDED
 #define MTEST_H_INCLUDED
 
+#define TRUE 1
+#define FALSE 0
 
-struct Test
+struct Expect
 {
-    int (* run)();
+    int expected;
+    int actual;
+    char* name;
 };
 
+typedef int (* testFunc)(struct Expect* exp);
+
+struct TestTable
+{
+    void* exp;
+    testFunc func;
+};
+
+extern struct TestTable* table;
+
+void addToTable(void* exp, testFunc func)
+{
+    int i;
+    for (i = 0; i < TESTMAX; i++)
+    {
+        if (table[i].exp != NULL)
+        {
+            table[i].exp = exp;
+            table[i].func = func;
+        }
+    }
+    if (i == TESTMAX)
+        printf("TestTable is full\n");
+    else
+        printf("TestTable is set up\n");
+}
+
+
+
+#define EXPECT_EQ(EXPECTED, ACTUAL) \
+struct Expect exp_##EXPECTED; \
+exp_##EXPECTED.name = #EXPECTED; \
+exp_##EXPECTED.expected = EXPECTED; \
+exp_##EXPECTED.actual = ACTUAL; \
+ \
+int run_##EXPECTED(struct Expect* e) \
+{ \
+    if (e->expected == e->actual) \
+    { \
+        printf("Expectation %s : PASS", e->name); \
+        return TRUE; \
+    else \
+    { \
+        printf("Expectation %s : FAIL\nExpected = %d\nActual = %d\b", e->name, e->expected, e->actual); \
+        return FALSE; \
+    } \
+} \
+ \
+addToTable(&exp_##EXPECTED, run_##EXPECTED);
+
+
+#define RUNTEST \
+do \
+{ \
+    int i = 0; \
+    for (i = 0; i < TESTMAX && TestTable[i].exp != NULL; i++) \
+        TestTable[i].func(TestTable[i].exp);\
+} while(0)
+
+
+
+#undef TRUE
+#undef FALSE
 
 #endif // MTEST_H_INCLUDED
