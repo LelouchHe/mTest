37075ccb648567985d6177d8d8345bf7fab40f42
Lelouch He <lelouchhe@gmail.com>
2012-03-11 16:29:03 +0800

    clean repo in fix branch

---
+++ mTestContext.h
@@ -0,0 +1,11 @@
+#ifndef MTESTCONTEXT_H_INCLUDED
+#define MTESTCONTEXT_H_INCLUDED
+
+struct TestContext
+{
+    struct TestSuite* pTs;
+    struct TestCase* pTc;
+};
+
+
+#endif // MTESTCONTEXT_H_INCLUDED
